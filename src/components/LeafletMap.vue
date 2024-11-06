6243b496c0e364a1e144db40b26f7960651bdaa5
cerciugur <ugur.cerci@yahoo.com>
2024-11-06 13:58:01 +0300

    Map url degisikligi

--- src/components/LeafletMap.vue
+++ src/components/LeafletMap.vue
@@ -1,12 +1,11 @@
 <template>
-  aaaaaaaaaaaaa
   <div id="myMap" style="width: 100%; height: 500px"></div>
 </template>
 
 <script>
 import { defineComponent } from "vue";
 import "leaflet/dist/leaflet.css";
-import * as L from "leaflet";
+import L from "leaflet";
 import { onMounted } from "vue";
 
 export default defineComponent({
@@ -20,7 +19,7 @@ onMounted(() => {
 
 function initMap() {
   var map = L.Map("myMap").setView([51.505, -0.09], 13);
-  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  L.tileLayer("https://tile.openstreetmap.org/{zoom}/{x}/{y}.png", {
     maxZoom: 19,
     attribution:
       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',

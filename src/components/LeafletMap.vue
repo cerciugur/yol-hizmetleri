eab78f94cef4c31a24a7f2b1c41d3da601af91eb
Sema Çerçi <sema.cerci1@gmail.com>
2024-11-08 19:37:33 +0300

    harita isaretcisi deneme

--- src/components/LeafletMap.vue
+++ src/components/LeafletMap.vue
@@ -5,33 +5,37 @@
 <script>
 import { onMounted, watch } from "vue";
 import L from "leaflet";
+
 import "leaflet/dist/leaflet.css";
 
 export default {
   setup() {
-    onMounted(() => initMap());
-
     function initMap() {
-      var map = new L.Map("myMap").setView([39.94, 32.802], 11);
+      var map = new L.Map("myMap").setView([39.94, 32.802], 13);
       L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        maxZoom: 20,
+        maxZoom: 19,
         attribution:
           '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
       }).addTo(map);
 
+      var marker = L.marker([39.94, 32.802]).addTo(map);
+      marker.bindPopup();
+
       map.locate({ setView: true, maxZoom: 40, watch: true, timeout: 6000 });
 
-      function getCurrentLocation(e) {
-        var radius = e.accuracy / 2;
-        L.marker(e.latlng)
-          .addTo(map)
-          .bindPopup("You are within " + radius + " meters").openPopup;
+      // function getCurrentLocation(e) {
+      //   var radius = e.accuracy / 2;
 
-        L.circle(e.latlng, radius).addTo(map);
-      }
+      //   L.marker(e.latlng)
+      //     .addTo(map)
+      //     .bindPopup("You are within " + radius + " meters")
+      //     .openPopup();
 
-      map.on("locationFound", getCurrentLocation);
+      //   L.circle([e.latlng, radius]).addTo(map);
+      // }
+      // map.on("click", getCurrentLocation);
     }
+    onMounted(() => initMap());
   },
 };
 </script>
